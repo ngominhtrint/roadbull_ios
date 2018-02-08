4916392c013069bf58af9d12d8d03dada1745c25
Tri Ngo <ngominhtrint@gmail.com>
2018-02-08 17:27:25 +0700

    Setup google map

--- Roadbull/AppDelegate.swift
+++ Roadbull/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyC-rnrTYHPNYzWTVSBJRfmXX9_-kYi3L5w")
+        
         return true
     }
 
--- Roadbull/ViewController.swift
+++ Roadbull/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        cameraZoomToCurrentLocation()
+        initialMarkers()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController {
+    
+    fileprivate func cameraZoomToCurrentLocation() {
+        let camera = GMSCameraPosition.camera(withLatitude: 10.79, longitude: 106.68, zoom: 17)
+        mapView.animate(to: camera)
+    }
+    
+    fileprivate func initialMarkers() {
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 10.7957471, longitude: 106.6876993)
+        marker.title = "The coffee house"
+        marker.snippet = "43 Hoa Hồng, Phường 25, Phú Nhuận, Hồ Chí Minh 700000, Việt Nam"
+        marker.appearAnimation = .pop
+        marker.map = mapView
+    }
+}

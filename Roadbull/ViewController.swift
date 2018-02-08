96762d0fa2e54bcc99c306dff491382610bc7a1b
Tri Ngo <ngominhtrint@gmail.com>
2018-02-08 12:15:30 +0700

    Initial project

---
+++ Roadbull/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roadbull
+//
+//  Created by TriNgo on 2/8/18.
+//  Copyright © 2018 TriNgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
